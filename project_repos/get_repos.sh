21d287daa527cd9ebff3e14f192f3ddb97c590c1
UsQuake <juju6985@outlook.kr>
2025-06-26 16:43:29 +0900

    Edit get_repos.sh.

--- project_repos/get_repos.sh
+++ project_repos/get_repos.sh
@@ -1,5 +1,7 @@
-set -e 
-ARCHIVE=ARCHIVE=defects4j-repos-v2.zip
+set -e
+
+ARCHIVE=defects4j-repos-v2.zip
+
 main(){
 # The BSD version of stat does not support --version or -c
     if stat --version &> /dev/null; then
